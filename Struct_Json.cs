a7c4808b9bf5bfb6643ccaeb5574c202811822d0
Microfish31 <75024370+Microfish31@users.noreply.github.com>
2021-10-03 21:36:10 +0800

    Add files via upload
    upload file

---
+++ Struct_Json.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System;
+
+
+namespace Struct_Json
+{
+    public class StructDewrapper<T>
+    {
+        public T[] items { get; set; }   // 不能改 items?
+    }
+
+    public class StructWrapper<T>
+    {
+        public T[] items { get; set; }  // 不能改 items?
+    }
+
+    public class StructJson
+    {
+
+        public T BytesToStruct<T>(byte[] data_byte)
+        {
+            Utf8JsonReader utf8Reader = new Utf8JsonReader(data_byte);
+            var data_struct = JsonSerializer.Deserialize<T>(ref utf8Reader);
+            return data_struct;
+        }
+
+        public T[] BytesToStructArray<T>(byte[] data_byte)
+        {
+            StructDewrapper<T> dewrap = new StructDewrapper<T>();
+            var utf8Reader = new Utf8JsonReader(data_byte);
+            dewrap = JsonSerializer.Deserialize<StructDewrapper<T>>(ref utf8Reader);
+            return dewrap.items;
+        }
+
+        public byte[] StructToBytes<T>(T data_struct)
+        {
+            byte[] jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(data_struct);
+            return jsonUtf8Bytes;
+        }
+
+        public byte[] StructToBytes<T>(T[] data_struct)
+        {
+            StructWrapper<T> wrap = new StructWrapper<T>();
+            wrap.items = data_struct;
+            byte[] jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(wrap);
+            return jsonUtf8Bytes;
+        }
+
+        public void StructFileWrite<T>(T[] data_struct, string path)
+        {
+            StructWrapper<T> wrap = new StructWrapper<T>();
+            wrap.items = data_struct;
+            byte[] jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(wrap);
+            File.WriteAllBytes(path, jsonUtf8Bytes);
+        }
+
+        public void StructFileWrite<T>(T data_struct, string path)
+        {
+            byte[] jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(data_struct);
+            File.WriteAllBytes(path, jsonUtf8Bytes);
+        }
+
+        public void BytesFileWrite(byte[] get_byte_array, string path) 
+        {
+            File.WriteAllBytes(path, get_byte_array);
+        }
+
+        public T FileReadStruct<T>(string path)
+        {
+            T type_data;
+
+            byte[] jsonUtf8Bytes = File.ReadAllBytes(path);
+            var utf8Reader = new Utf8JsonReader(jsonUtf8Bytes);
+            type_data = JsonSerializer.Deserialize<T>(ref utf8Reader);
+            return type_data;
+        }
+
+        public T[] FileReadStructArray<T>(string path)
+        {
+            StructDewrapper<T> dewrap = new StructDewrapper<T>();
+            byte[] jsonUtf8Bytes = File.ReadAllBytes(path);
+            var utf8Reader = new Utf8JsonReader(jsonUtf8Bytes);
+            dewrap = JsonSerializer.Deserialize<StructDewrapper<T>>(ref utf8Reader);
+            return dewrap.items;
+        }
+    }
+}
---
+++ Unity_Data.cs
@@ -0,0 +1,103 @@
+﻿namespace Unity_Data
+{
+    public class Position
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public void Position_Set(float x, float y, float z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+
+    public class Rotation
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float W { get; set; }
+
+        public void Rotation_Set(float x, float y, float z, float w)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+            this.W = w;
+        }
+
+    }
+
+    public class UnityClientStruct
+    {
+
+        public string Objname { get; set; }
+        public string Ip { get; set; }
+        public int Port { get; set; }
+
+        public Position Position { get; set; }
+
+        public Rotation Rotation { get; set; }
+        public string Color { get; set; }
+    }
+
+    public class UnityClientData
+    {
+        public UnityClientStruct Data = new UnityClientStruct();
+        private Position pos = new Position();
+        private Rotation rot = new Rotation();
+        public string File_path = null;
+
+        public void Data_ini(string objname,string ip,int port)
+        {
+            pos.Position_Set(0, 0, 0);
+            rot.Rotation_Set(0, 0, 0, 0);
+
+            Data.Objname = objname;
+            Data.Ip = ip;
+            Data.Port = port;
+            Data.Position = pos;
+            Data.Rotation = rot;
+            Data.Color = "#2828FF";
+        }
+    }
+
+    public class UnityObjectStruct
+    {
+        public string Objname { get; set; }
+        public string Ip { get; set; }
+        public int Port { get; set; }
+
+        public Position Position { get; set; }
+
+        public Rotation Rotation { get; set; }
+        public string Color { get; set; }
+
+        public bool Enable { get; set; }
+    }
+
+    public class UnityObjectData
+    {
+        public UnityObjectStruct Data = new UnityObjectStruct();
+        private Position pos = new Position();
+        private Rotation rot = new Rotation();
+        public string File_path = null;
+
+        public void Data_ini(string objname, string ip, int port)
+        {
+            pos.Position_Set(0, 0, 0);
+            rot.Rotation_Set(0, 0, 0, 0);
+
+            Data.Objname = objname;
+            Data.Ip = ip;
+            Data.Port = port;
+            Data.Position = pos;
+            Data.Rotation = rot;
+            Data.Color = "#2828FF";
+            Data.Enable = false;
+        }
+    }
+}
